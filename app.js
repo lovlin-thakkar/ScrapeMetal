bd00b09be8d5be2e56af102fc0b52735cef1eec7
Lovlin Thakkar <lovlinthakkar99@gmail.com>
2019-09-04 18:11:46 +0530

    set heroku port

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.set(process.env.PORT);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
